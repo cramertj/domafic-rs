457622348b8ce58b999ae8f34a940ce5fef1cc9a
Taylor Cramer <cramertj@cs.washington.edu>
2016-12-15 16:48:08 -0800

    Implement traits for DOM visitor API

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,87 @@
+// Temporary
+#![allow(dead_code)]
+
+trait DOMNode {}
+
+trait DOMLevel {
+    type NodeType: DOMNode;
+    type ChildrenType: DOMChildren;
+}
+
+trait DOMChildrenProcessor {
+    type State;
+    fn get_processor<T: DOMLevel>() -> fn(&mut Self::State, T) -> ();
+}
+
+// Dummy type for testing
+struct ChildCounter;
+impl DOMChildrenProcessor for ChildCounter {
+    type State = usize;
+
+    fn get_processor<T: DOMLevel>() -> fn(&mut Self::State, T) -> () {
+        fn incr<T: DOMLevel>(state: &mut usize, _level: T) {
+            *state += 1;
+        }
+        incr
+    }
+}
+
+trait DOMChildren {
+    fn process_all<P: DOMChildrenProcessor>(&self, processor: &mut P::State) -> ();
+}
+
+// Credit to @shepmaster for structure of recursive tuple macro
+macro_rules! tuple_impls {
+    () => {};
+
+    // Copywrite @shepmaster
+    (($idx:tt => $typ:ident), $( ($nidx:tt => $ntyp:ident), )*) => {
+        /*
+         * Invoke recursive reversal of list that ends in the macro expansion implementation
+         * of the reversed list
+        */
+        tuple_impls!([($idx, $typ);] $( ($nidx => $ntyp), )*);
+        tuple_impls!($( ($nidx => $ntyp), )*); // invoke macro on tail
+    };
+
+    /*
+     * ([accumulatedList], listToReverse); recursively calls tuple_impls until the list to reverse
+     + is empty (see next pattern)
+    */
+    ([$(($accIdx: tt, $accTyp: ident);)+]  ($idx:tt => $typ:ident), $( ($nidx:tt => $ntyp:ident), )*) => {
+      tuple_impls!([($idx, $typ); $(($accIdx, $accTyp); )*] $( ($nidx => $ntyp), ) *);
+    };
+
+    // Finally expand into the implementation
+    ([($idx:tt, $typ:ident); $( ($nidx:tt, $ntyp:ident); )*]) => {
+        impl<$typ, $( $ntyp ),*> DOMChildren for ($typ, $( $ntyp ),*)
+            where $typ: DOMLevel + Copy,
+                  $( $ntyp: DOMLevel + Copy ),*
+        {
+            fn process_all<P>(&self, state: &mut P::State) -> ()
+                    where P: DOMChildrenProcessor {
+                P::get_processor()(state, self.$idx);
+                $(
+                    P::get_processor()(state, self.$nidx);
+                )*
+            }
+        }
+    }
+}
+
+tuple_impls!(
+    (9 => J),
+    (8 => I),
+    (7 => H),
+    (6 => G),
+    (5 => F),
+    (4 => E),
+    (3 => D),
+    (2 => C),
+    (1 => B),
+    (0 => A),
+);
+
 #[cfg(test)]
 mod tests {
     #[test]
