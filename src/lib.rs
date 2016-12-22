7fde73f83c9ac7a80a8930a787da4de6c4276b0d
Taylor Cramer <cramertj@cs.washington.edu>
2016-12-22 03:27:34 -0800

    First successful compilation of gross listener mapping processor

--- src/lib.rs
+++ src/lib.rs
@@ -350,14 +350,14 @@ pub trait Listener {
     fn handle_event(&self, event: events::Event) -> Self::Message;
 }
 
-pub struct MappedListener<M, L: Listener, F: Fn(L::Message) -> M>(L, F, PhantomData<M>);
-impl<M, L: Listener, F: Fn(L::Message) -> M> Listener for MappedListener<M, L, F> {
+pub struct MappedListener<'a, M, L: Listener + 'a, F: Map<L::Message, Out=M>>(&'a L, PhantomData<(M, F)>);
+impl<'a, M, L: Listener, F: Map<L::Message, Out=M>> Listener for MappedListener<'a, M, L, F> {
     type Message = M;
-    fn event_types_handled<'a>() -> &'a [events::EventType] {
+    fn event_types_handled<'b>() -> &'b [events::EventType] {
         L::event_types_handled()
     }
     fn handle_event(&self, event: events::Event) -> Self::Message {
-        self.1(self.0.handle_event(event))
+        F::map(self.0.handle_event(event))
     }
 }
 
@@ -376,6 +376,30 @@ pub trait ListenerProcessor<Message> {
     ///
     /// TODO: Example
     fn get_processor<T: Listener<Message=Message>>() -> fn(&mut Self::Acc, &T) -> Result<(), Self::Error>;
+}
+
+pub trait Map<In> {
+    type Out;
+    fn map(input: In) -> Self::Out;
+}
+
+pub struct MappedListenerProcessor<OldM, NewM, L: ListenerProcessor<OldM>, F: Map<NewM, Out=OldM>>
+    (L, F, PhantomData<(OldM, NewM)>);
+
+impl<OldM, NewM, L: ListenerProcessor<OldM>, F: Map<NewM, Out=OldM>> ListenerProcessor<NewM>
+    for MappedListenerProcessor<OldM, NewM, L, F>
+{
+    type Acc = L::Acc;
+    type Error = L::Error;
+    fn get_processor<T: Listener<Message=NewM>>() -> fn(&mut Self::Acc, &T) -> Result<(), Self::Error> {
+        fn process_mapped<OldM, NewM, L: ListenerProcessor<OldM>, F: Map<NewM, Out=OldM>,
+            T: Listener<Message=NewM>>(acc: &mut L::Acc, listener: &T)
+            -> Result<(), L::Error>
+        {
+            L::get_processor()(acc, &MappedListener::<OldM, T, F>(listener, PhantomData))
+        }
+        process_mapped::<OldM, NewM, L, F, T>
+    }
 }
 
 pub trait Listeners {
